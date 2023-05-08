c9685be97183fcf45c4472e6daede3eb0b53496e
HarllonCS <harllon.santana@gmail.com>
2023-05-08 11:06:03 -0300

    PHP
    organized

---
+++ tests/PHP/superglobals/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Superglobals on PHP</title>
+</head>
+<body>
+    <main>
+        <pre>
+            <?php 
+                echo "<h1>Superglobal GET</h1>";
+                var_dump($_GET);
+            ?>
+        </pre>
+    </main>
+</body>
+</html>
