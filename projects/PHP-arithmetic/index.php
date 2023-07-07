ec63673c9739b4a7cac117bfc74c2cec263695b2
HarllonCS <harllon.santana@gmail.com>
2023-07-06 23:57:48 -0300

    PHP projects
    Arithmetics

--- projects/PHP-arithmetic/index.php
+++ projects/PHP-arithmetic/index.php
@@ -5,43 +5,50 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edhe">
 	<meta name="viewport" cotnent="width=device=width, initial-scale=1">
 	<title>Arithmetic Avarage</title>
+	
+	<link rel="stylesheet" href="./style.css">
 </head>
 <body>
-	<main role="main">
+	<main role="main"> <!-- Main content box -->
+		<!-- Title -->
 		<h1>Arithmetic Avarage</h1>
 		
 		<form action="<?=$_SERVER['PHP_SELF']?>" method="get">
-			<div class="box">
-				<label for="v1">1° Value</label>
+			<div class="box"> <!-- 1° Value field -->
+				<label for="v1"><span class="lb-n">1</span>° Value</label>
 				<input type="number" id="v1" autofocus>
-			</div>
+			</div> <!-- 1° Value field /end -->
 			
-			<div class="box">
-				<label for="w1">1° Weight</label>
+			<div class="box"> <!-- 1° Weight field -->
+				<label for="w1"><span class="lb-n">1</span>° Weight</label>
 				<input type="number" id="v1">
-			</div>
+			</div> <!-- 1° Weight field /end -->
 			
-			<div class="box">
-				<label for="v2">2° Value</label>
+			<div class="box"> <!-- 2° Value field -->
+				<label for="v2"><span class="lb-n">2</span>° Value</label>
 				<input type="number" id="v1">
-			</div>
+			</div> <!-- 2° Value field /end -->
 			
-			<div class="box">
-				<label for="w2">2° Weight</label>
+			<div class="box"> <!-- 2° Weight field -->
+				<label for="w2"><span class="lb-n">2</span>° Weight</label>
 				<input type="number" id="v1">
-			</div>
-			
-			<button type="submit">Calculate Avarage</button>
+			</div> <!-- 2° Weight field /end -->
 		</form>
-	</main>
+		
+		<div id="btn"> <!-- Calculate Button -->
+			<button type="submit">Calculate Avarage</button>
+		</div> <!-- Calculate Button /end -->
+	</main> <!-- Main content box /end -->
 	
-	<section>
+	<section> <!-- Results box -->
 		<h2>Results</h2>
 		
-		<?php
-			echo "<p>Simple Arithmetic mean = </p>"
-			echo "<p>Weighted arithmetic mean = </p>"
-		?>
-	</section>
+		<div id="res">
+			<?php
+				echo "<p>Simple Arithmetic mean = </p>";
+				echo "<p>Weighted arithmetic mean = </p>";
+			?>
+		</div>
+	</section> <!-- Results box /end -->
 </body>
 </html>
