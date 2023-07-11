078191d07eb35c21a6b963f4ddb21337ba728d5f
HarllonCS <harllon.santana@gmail.com>
2023-07-11 12:21:18 -0300

    PHP Projects
    PHP Time

--- projects/PHP_12-time/index.php
+++ projects/PHP_12-time/index.php
@@ -9,15 +9,29 @@
 	<link rel="stylesheet" href="./style.css">
 </head>
 <body>
-	<?php $sec = $_GET['sec'] ?? 0 ?>
+	<?php
+		$sec = $_GET['sec'] ?? 0;
+		// Week
+		$weekDiv = intdiv($sec, 604800);
+		$weekRest = $sec % 604800;
+		// Day
+		$dayDiv = intdiv($weekRest, 86400);
+		$dayRest = $weekRest % 86400;
+		// Hours
+		$hourDiv = intdiv($dayRest, 3600);
+		$hourRest = $dayRest % 3600;
+		// Minutes
+		$minDiv = intdiv($hourRest, 60);
+		$minRest = $hourRest % 60;
+	?>
 	
 	<main role="main">
 		<h1>Time Calculator</h1>
 		
 		<form action="<?=$_SERVER['PHP_SELF']?>" method="get">
 			<div style="margin-bottom: 16px;">
-			<label for="sec">Seconds</label>
-			<input type="number" id="sec" name="sec" min="1" value="<?=$sec?>" autofocus>
+				<label for="sec">Seconds</label>
+				<input type="number" id="sec" name="sec" min="1" value="<?=$sec?>" step="1" autofocus>
 			</div>
 			
 			<button type="submit">Calculate</button>
@@ -27,15 +41,13 @@
 	<section>
 		<h2>Results</h2>
 		
-		<div id="res">
-			<?php
-				echo "<p>Semanas = <span></span></p>";
-				echo "<p>Dias = <span></span></p>";
-				echo "<p>Horas = <span></span></p>";
-				echo "<p>Minutos = <span></span></p>";
-				echo "<p>Segundos = <span></span></p>";
-			?>
-		</div>
+		<ul>
+			<li>Weeks = <strong><?=$weekDiv?></strong></li>
+			<li>Days = <strong><?=$dayDiv?></strong></li>
+			<li>Hours = <strong><?=$hourDiv?></strong></li>
+			<li>Minutes = <strong><?=$minDiv?></strong></li>
+			<li>Seconds = <strong><?=$minRest?></strong></li>
+		</ul>
 	</section>
 </body>
 </html>
